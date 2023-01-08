9a860e09b72df451809e58e56c1403cedb8b821a
Rudolf Garstenauer <rgarstenauer@yahoo.com>
2023-01-08 16:58:23 +0100

    08.01.2023 17:00  Timer Class

--- Assets/Scripts/UI/PlayerUI.cs
+++ Assets/Scripts/UI/PlayerUI.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using UI;
 using UnityEngine;
 
 public class PlayerUI : MonoBehaviour
@@ -8,29 +9,20 @@ public class PlayerUI : MonoBehaviour
     private GameManager gm;
     
     private float _startTime;
+    // private float time_score;
+    private TimeScore timeScore;  
 
     private void Start()
     {
-        _startTime = Time.time; 
+        _startTime = Time.time;
+        timeScore = new TimeScore();
         gm = FindObjectOfType<GameManager>(); 
     }
 
     private void FixedUpdate()
     {
-        float t = Time.time - _startTime;
-        
-        int hrs = 00 ;
-        int min = (int)t / 60;
-        float sec = t % 60;
-
-        if (min >= 60)
-        {
-            hrs = min / 60; 
-            min %= 60;
-        }
-        
-        timerField.text = hrs + ":" + min + ":" + sec.ToString("f2");
-        livesText.text = gm.GetLives() + " Lives"; 
-
+       timeScore.SetTimeScore(Time.time - _startTime);
+       timerField.text = timeScore.GetString();
+       livesText.text = gm.GetLives() + " Lives";
     }
 }
---
+++ Assets/Scripts/UI/TimeScore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace UI
+{
+    public class TimeScore
+    {
+        private float time_score;
+
+        public TimeScore()
+        {
+            time_score = 0f; 
+        }
+
+        public float GetTimeScore()
+        {
+            return time_score;
+        }
+        
+        public void SetTimeScore(float newTime)
+        {
+            time_score= newTime;
+        }
+
+        public String GetString()
+        {
+            int hrs = 00 ;
+            int min = (int)time_score / 60;
+            float sec = time_score % 60;
+
+            if (min >= 60)
+            {
+                hrs = min / 60; 
+                min %= 60;
+            }
+
+            return hrs + ":" + min + ":" + sec.ToString("f2");
+        }
+    }
+}
