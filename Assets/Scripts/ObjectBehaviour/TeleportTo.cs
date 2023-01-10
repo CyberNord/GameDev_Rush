17976cf7d946810882c4208a75b760c355bd303b
Rudolf Garstenauer <rgarstenauer@yahoo.com>
2023-01-10 01:00:14 +0100

    10.01.23 01:00 TeleporterRotation

--- Assets/Scripts/ObjectBehaviour/TeleportTo.cs
+++ Assets/Scripts/ObjectBehaviour/TeleportTo.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 public class TeleportTo : MonoBehaviour
 {
     [SerializeField] private GameObject teleportDestination;
-    
+    [SerializeField] private GameObject Hero;
+    [SerializeField] private float RotateX;
+    [SerializeField] private float RotateY;
+    [SerializeField] private float RotateZ;
+
     private Vector3 _newPos;
 
     // Start is called before the first frame update
@@ -15,5 +19,6 @@ public class TeleportTo : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         collision.transform.position = _newPos;
+        collision.transform.Rotate(RotateX, RotateY, RotateZ);
     }
 }
